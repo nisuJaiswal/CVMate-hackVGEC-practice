ab1780f50ecc1a4060e5465e9e987f6f94344e53
Nisarg Jaiswal <nisujaiswal4@gmail.com>
2023-03-14 22:08:38 +0530

    cors and proxy added

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ const { errorHandler } = require('./middleware/errorHandler')
 const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 4000
-
+const cors = require('cors')
 connectDB()
 
 app.use('/uploadFolder', express.static('backend/uploadFolder'))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
